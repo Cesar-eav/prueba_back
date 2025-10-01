00b871b88a210cd93e7f8ae8893b03140fd1f4cb
Cesar-eav <cesar.eav@hotmail.es>
2025-09-30 23:37:02 -0300

    Nuevas vistas

--- tareas/views.py
+++ tareas/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView,DeleteView
+from .models import Tarea
 
 # Create your views here.
+class TareaListView(ListView):
+    model = Tarea
+    template_name = "tarea/lista.html"
+    context_object_name = "tareas"
+
+class TareaCreateView(CreateView):
+    model = Tarea
+    fields = ["titulo", "descripcion", "prioridad", "vigente", "fecha_limite"]
+    template_name = "tarea/form.html"
+    success_url = reverse_lazy("tarea_lista")
+
+class TareaUpdateView(UpdateView):
+    model = Tarea
+    fields = ["titulo", "descripcion", "prioridad", "vigente", "fecha_limite"]
+    template_name = "tarea/form.html"
+    success_url = reverse_lazy("tarea_lista")
+
+class TareaDeleteView(DeleteView):
+    model = Tarea
+    template_name = "tarea/confirmar_eliminar.html"
+    success_url = reverse_lazy("tarea_lista")
